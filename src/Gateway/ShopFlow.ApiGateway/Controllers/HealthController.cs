056b7bd4038963537423545c2156a0a66e2d9a5a
srirakshathirumali <srirakshathirumali@gmail.com>
2026-05-09 17:12:48 -0700

    YARP API Gateway routing all services

---
+++ src/Gateway/ShopFlow.ApiGateway/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopFlow.ApiGateway.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() =>
+        Ok(new
+        {
+            status = "Healthy",
+            service = "ShopFlow API Gateway",
+            timestamp = DateTime.UtcNow
+        });
+}
